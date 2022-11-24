59b6282c526e21082f69cb0337798d4cb05bd683
Bryn Llewellyn <bryn@yugabyte.com>
2022-11-23 16:01:15 -0800

    Third substantive PR. W.i.P. creating the initial content before making public (#3)

--- triggers/mandatory-one-to-many-relationship/ask-tom-approach/20-cr-triggers.sql
+++ triggers/mandatory-one-to-many-relationship/ask-tom-approach/20-cr-triggers.sql
@@ -1,4 +1,29 @@
 set role d4$data;
+/*
+  Workaround for limitation that YB does not yet support
+    "referencing old table as old_details"
+  for an "after statement" trigger.
+*/
+create function data.details_after_row()
+  returns trigger
+  security definer
+  set search_path = pg_catalog, pg_temp
+  language plpgsql
+as $body$
+declare
+  mk_ constant int not null := old.mk;
+begin
+  create temporary table if not exists old_details(mk int);
+  insert into old_details(mk) values(mk_);
+  return old;
+end;
+$body$;
+
+create trigger details_after_row
+  after delete
+  on data.details
+  for each row
+execute function data.details_after_row();
 
 create type data.mk_mdk_ddks as (mk int, m_dk int, d_dks int[]);
 
@@ -83,11 +108,22 @@ begin
 end;
 $body$;
 
---/*
+/*
+  The natural approach is to use "referencing old table as old_details"
+  so that the RDMBS populates is automaically. However, YB (at least
+  through YB-2.15.3.2) doesn't yet support this. See
+
+    https://github.com/yugabyte/yugabyte-db/issues/1668
+
+  We the therefore need to create and populate the "old_details"
+  temporary table with explicit application code.
+
+  See "create trigger details_after_row" above and the trigger function
+  that it uses.
+*/;
 create trigger details_after_statement
   after delete
   on data.details
-  referencing old table as old_details
+  /* referencing old table as old_details */
   for each statement
 execute function data.details_after_statement();
---*/;
